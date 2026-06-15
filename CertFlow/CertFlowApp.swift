9d29fc75929e30ca85533c269a99dcd8b44c00ea
GAO <gaoxiaoxingok@gmail.com>
2026-06-15 10:51:10 +0800

    Initial Commit

---
+++ CertFlow/CertFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertFlowApp.swift
+//  CertFlow
+//
+//  Created by GAO on 2026/6/15.
+//
+
+import SwiftUI
+
+@main
+struct CertFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CertFlowTests/CertFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  CertFlowTests.swift
+//  CertFlowTests
+//
+//  Created by GAO on 2026/6/15.
+//
+
+import Testing
+@testable import CertFlow
+
+struct CertFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
